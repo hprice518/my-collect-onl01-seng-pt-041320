8d7011a684eadaa80599bdc816880402b0f236bb
Harrison Price <hprice518@gmail.com>
2020-04-26 15:51:05 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,14 @@
 
 def my_collect(array)
   i = 0 
-  collection = [ ] 
+  names = [ ] 
+  languages = [ ]
   while i < array.length
-  my_collect(collection)  {|name| name.split(" ").first} 
-  
+  my_collect(names)  {|name| name.split(" ").first} 
+  my_collect(languages)  {|lang| languages.upcase}
   i += 1 
   end
-
+my_collect(names)
+my_collect(languages)
 end 
 
